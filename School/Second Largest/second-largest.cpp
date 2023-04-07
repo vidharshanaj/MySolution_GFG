9147e5fae5533560abeae0bb823a6c99be6f9581
VIDHARSHANA J <106165614+vidharshanaj@users.noreply.github.com>
2023-04-07 15:52:03 +0530

    Updated Solution - GfG to GitHub

--- School/Second Largest/second-largest.cpp
+++ School/Second Largest/second-largest.cpp
@@ -9,21 +9,20 @@ class Solution{
 public:	
 	// Function returns the second largest elements
 	int print2largest(int arr[], int n) {
-      int largest =arr[0], secondLargest =-1;
-
-      for(int i = 1; i <n; i++) 
-      {
-      if(arr[i]>largest)
-        largest = arr[i];
-      }
-      for(int i = 0; i <n; i++)
-       {
-        if(arr[i]>secondLargest && arr[i]!=largest)
-        {
-           secondLargest = arr[i];
-        }
-       }
-    return secondLargest;
+     int largest = arr[0], secondLargest = -1;
+     
+     for(int i= 1;i<n;i++)
+     {
+         if(arr[i]>largest)
+         {
+             secondLargest = largest;
+             largest = arr[i];  
+         }
+         
+         else if(arr[i]<largest && arr[i]>secondLargest)
+            secondLargest = arr[i];
+     }
+     return secondLargest;
 	}
 };
 
